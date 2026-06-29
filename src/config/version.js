93901f987645091603c4517040b841ba41dee5e5
Markides.Markellos <markellos.markides@gmail.com>
2026-06-29 23:26:08 +0300

    Update version.js

--- src/config/version.js
+++ src/config/version.js
@@ -1,5 +1,5 @@
-export const APP_VERSION = 'v0.1.0';
-export const BUILD_TIMESTAMP = '29062026_1433';
-export const COMMIT_HASH = '126b885';
+export const APP_VERSION = 'v0.2.0';
+export const BUILD_TIMESTAMP = '29062026_2325';
+export const COMMIT_HASH = 'c339658';
 export const APP_VERSION_LABEL =
   `${APP_VERSION} \u00B7 ${BUILD_TIMESTAMP} \u00B7 ${COMMIT_HASH}`;
