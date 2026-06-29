050304b8891835ebb22d2a8513f78e850dcce213
Markides.Markellos <markellos.markides@gmail.com>
2026-06-29 14:38:40 +0300

    Update release metadata for v0.1.0

--- src/config/version.js
+++ src/config/version.js
@@ -1,5 +1,5 @@
 export const APP_VERSION = 'v0.1.0';
-export const BUILD_TIMESTAMP = '29062026_1246';
-export const COMMIT_HASH = 'dev';
+export const BUILD_TIMESTAMP = '29062026_1433';
+export const COMMIT_HASH = '126b885';
 export const APP_VERSION_LABEL =
   `${APP_VERSION} \u00B7 ${BUILD_TIMESTAMP} \u00B7 ${COMMIT_HASH}`;
